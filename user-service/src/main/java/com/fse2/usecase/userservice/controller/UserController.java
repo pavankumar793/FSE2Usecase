9a08c6134476da9f3c053aaaefd160294227dd3b
pavankumar793 <chivukulapavankumar5.com>
2024-05-31 01:54:53 +0530

    Remove unused imports 	modified:   user-service/src/main/java/com/fse2/usecase/userservice/controller/UserController.java

--- user-service/src/main/java/com/fse2/usecase/userservice/controller/UserController.java
+++ user-service/src/main/java/com/fse2/usecase/userservice/controller/UserController.java
@@ -1,16 +1,10 @@
 package com.fse2.usecase.userservice.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
