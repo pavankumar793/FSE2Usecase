a194330bec266e47c302257f67f8f4cfb418315b
pavankumar793 <chivukulapavankumar5.com>
2024-07-14 20:48:33 +0530

    Stop sending logs by kafka for time being

--- user-service/src/main/java/com/fse2/usecase/userservice/config/KafkaTopicConfig.java
+++ user-service/src/main/java/com/fse2/usecase/userservice/config/KafkaTopicConfig.java
@@ -1,15 +1,31 @@
-package com.fse2.usecase.userservice.config;
+// package com.fse2.usecase.userservice.config;
 
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
+// import org.apache.kafka.clients.admin.NewTopic;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.kafka.config.TopicBuilder;
 
-@Configuration
-public class KafkaTopicConfig {
-    @Bean
-    public NewTopic blogSiteApplicationTopic() {
-        return TopicBuilder.name("blogSiteApplicationTopic")
-                .build();
-    }
-}
+// @Configuration
+// public class KafkaTopicConfig {
+//     @Bean
+//     public NewTopic blogSiteApplicationTopic() {
+//         return TopicBuilder.name("blogSiteApplicationTopic")
+//                 .build();
+//     }
+// }
+
+// <configuration>
+//     <appender name="KAFKA" class="com.github.danielwegener.logback.kafka.KafkaAppender">
+//         <encoder class="ch.qos.logback.classic.encoder.PatternLayoutEncoder">
+//             <pattern>%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n</pattern>
+//         </encoder>
+//         <topic>blogSiteApplicationTopic</topic>
+//         <keyingStrategy class="com.github.danielwegener.logback.kafka.keying.NoKeyKeyingStrategy"/>
+//         <deliveryStrategy class="com.github.danielwegener.logback.kafka.delivery.AsynchronousDeliveryStrategy"/>
+//         <producerConfig>bootstrap.servers=localhost:9092</producerConfig>
+//     </appender>
+
+//     <root level="INFO">
+//         <appender-ref ref="KAFKA" />
+//     </root>
+// </configuration>
