30bbb6ae87bff7d0b0d523dc87faee041a89b09f
pavankumar793 <chivukulapavankumar5.com>
2024-07-19 13:20:26 +0000

    remove filters in gateway

--- app-gateway/src/main/java/com/fse2/usecase/applicationgateway/filter/ErrorFilter.java
+++ app-gateway/src/main/java/com/fse2/usecase/applicationgateway/filter/ErrorFilter.java
@@ -1,39 +1,39 @@
-package com.fse2.usecase.applicationgateway.filter;
+// package com.fse2.usecase.applicationgateway.filter;
 
-import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.stereotype.Component;
 
-import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
+// import com.netflix.zuul.ZuulFilter;
+// import com.netflix.zuul.context.RequestContext;
 
-@Component
-public class ErrorFilter extends ZuulFilter {
-	private static Logger log = LoggerFactory.getLogger(PostFilter.class);
+// @Component
+// public class ErrorFilter extends ZuulFilter {
+// 	private static Logger log = LoggerFactory.getLogger(PostFilter.class);
 
-	  @Override
-	  public String filterType() {
-	    return "error";
-	  }
+// 	  @Override
+// 	  public String filterType() {
+// 	    return "error";
+// 	  }
 
-	  @Override
-	  public int filterOrder() {
-	    return 1;
-	  }
+// 	  @Override
+// 	  public int filterOrder() {
+// 	    return 1;
+// 	  }
 
-	  @Override
-	  public boolean shouldFilter() {
-	    return true;
-	  }
+// 	  @Override
+// 	  public boolean shouldFilter() {
+// 	    return true;
+// 	  }
 
-	  @Override
-	  public Object run() {
-	    HttpServletResponse response = RequestContext.getCurrentContext().getResponse();
+// 	  @Override
+// 	  public Object run() {
+// 	    HttpServletResponse response = RequestContext.getCurrentContext().getResponse();
 	    
-	    log.info("ErrorFilter: " + String.format("response status is %d", response.getStatus()));
+// 	    log.info("ErrorFilter: " + String.format("response status is %d", response.getStatus()));
 	    
-	    return null;
-	  }
-}
+// 	    return null;
+// 	  }
+// }
--- app-gateway/src/main/java/com/fse2/usecase/applicationgateway/filter/PostFilter.java
+++ app-gateway/src/main/java/com/fse2/usecase/applicationgateway/filter/PostFilter.java
@@ -1,39 +1,39 @@
-package com.fse2.usecase.applicationgateway.filter;
+// package com.fse2.usecase.applicationgateway.filter;
 
-import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.stereotype.Component;
 
-import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
+// import com.netflix.zuul.ZuulFilter;
+// import com.netflix.zuul.context.RequestContext;
 
-@Component
-public class PostFilter extends ZuulFilter {
-	private static Logger log = LoggerFactory.getLogger(PostFilter.class);
+// @Component
+// public class PostFilter extends ZuulFilter {
+// 	private static Logger log = LoggerFactory.getLogger(PostFilter.class);
 
-	  @Override
-	  public String filterType() {
-	    return "post";
-	  }
+// 	  @Override
+// 	  public String filterType() {
+// 	    return "post";
+// 	  }
 
-	  @Override
-	  public int filterOrder() {
-	    return 1;
-	  }
+// 	  @Override
+// 	  public int filterOrder() {
+// 	    return 1;
+// 	  }
 
-	  @Override
-	  public boolean shouldFilter() {
-	    return true;
-	  }
+// 	  @Override
+// 	  public boolean shouldFilter() {
+// 	    return true;
+// 	  }
 
-	  @Override
-	  public Object run() {
-	    HttpServletResponse response = RequestContext.getCurrentContext().getResponse();
+// 	  @Override
+// 	  public Object run() {
+// 	    HttpServletResponse response = RequestContext.getCurrentContext().getResponse();
 	    
-	    log.info("PostFilter: " + String.format("response's content type is %s", response.getStatus()));
+// 	    log.info("PostFilter: " + String.format("response's content type is %s", response.getStatus()));
 	    
-	    return null;
-	  }
-}
+// 	    return null;
+// 	  }
+// }
