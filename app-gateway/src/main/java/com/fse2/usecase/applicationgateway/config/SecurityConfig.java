464011c40f6f239839687dfba3d3dc3f9085ec4e
pavankumar793 <chivukulapavankumar5.com>
2024-07-20 18:42:58 +0000

    Remove changes for CORS error

--- app-gateway/src/main/java/com/fse2/usecase/applicationgateway/config/SecurityConfig.java
+++ app-gateway/src/main/java/com/fse2/usecase/applicationgateway/config/SecurityConfig.java
@@ -5,16 +5,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/blogsite/**", "/actuator/**").permitAll()
-                .anyRequest().authenticated().and()
-                .exceptionHandling().and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeRequests(requests -> requests.antMatchers("/blogsite/**", "/actuator/**").permitAll()
+                        .anyRequest().authenticated())
+                .exceptionHandling(exceptionHandling -> {
+                    exceptionHandling.authenticationEntryPoint(new Http403ForbiddenEntryPoint());
+                    exceptionHandling.accessDeniedHandler(new AccessDeniedHandlerImpl());
+                })
+                .sessionManagement(management -> management
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     }
 }
