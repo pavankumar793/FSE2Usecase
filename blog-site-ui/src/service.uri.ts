36292f3a3cf0170c0b7d67b0eae5b7ee495dc83f
pavankumar793 <chivukulapavankumar5.com>
2024-07-20 19:05:41 +0000

    Remove cors config from mcs

--- blog-site-ui/src/service.uri.ts
+++ blog-site-ui/src/service.uri.ts
@@ -1,7 +1,7 @@
 export const SERVICE_URI = {
     register: 'https://fse2appgateway.azurewebsites.net/blogsite/user/register',
     login: 'https://fse2appgateway.azurewebsites.net/blogsite/user/login',
-    searchByCategory: 'https://fse2blogservice.azurewebsites.net/blogsite/search/',
+    searchByCategory: 'https://fse2appgateway.azurewebsites.net/blogsite/search/',
     getBlogsByUser: 'https://fse2appgateway.azurewebsites.net/blogsite/blogs/getAllOfUser/',
     addBlog: 'https://fse2appgateway.azurewebsites.net/blogsite/blogs/add',
     deleteBlog: 'https://fse2appgateway.azurewebsites.net/blogsite/blogs/delete/',
--- user-service/src/main/java/com/fse2/usecase/userservice/config/WebConfig.java
+++ user-service/src/main/java/com/fse2/usecase/userservice/config/WebConfig.java
@@ -1,21 +1,21 @@
-package com.fse2.usecase.userservice.config;
+// package com.fse2.usecase.userservice.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+// @Configuration
+// @EnableWebMvc
+// public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://agreeable-smoke-0160ae30f.5.azurestaticapps.net")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins("https://agreeable-smoke-0160ae30f.5.azurestaticapps.net")
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                 .allowedHeaders("*")
+//                 .allowCredentials(true);
+//     }
+// }
